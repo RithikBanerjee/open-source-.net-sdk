4fe4646c80bb228dc3b866ed2e5fec6c733a55cc
Rithik Banerjee <banerjee13rithik@gmail.com>
2020-10-09 10:58:53 +0530

    [feature] comment helper

--- GSTAPI/Models/Response.cs
+++ GSTAPI/Models/Response.cs
@@ -1,12 +1,14 @@
-﻿
+
 namespace GSTAPI.Models
 {
+    //basic response given to user
     public class Response
     {
         public bool Status { get; set; }
         public string OutcomeTransaction { get; set; }
         public string Data { get; set; }
     }
+    //authentication response given to user
     public class AuthResponse
     {
         public bool Status { get; set; }
