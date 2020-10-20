1455b30612b5715e5a90699a53ff3432890cb21e
Rithik Banerjee <banerjee13rithik@gmail.com>
2020-10-20 09:38:26 +0530

    [feature] comment helper

--- GSTAPI/Services/GSTR4AService.cs
+++ GSTAPI/Services/GSTR4AService.cs
@@ -1,9 +1,10 @@
-﻿using GSTAPI.Helper;
+using GSTAPI.Helper;
 using GSTAPI.Models;
 using System.Collections.Specialized;
 
 namespace GSTAPI.Services
 {
+    //class to call all gstr4a based api 
     public static class GSTR4AService
     {
         private static Response GetInvoices(Request userInfo, NameValueCollection queryString)
