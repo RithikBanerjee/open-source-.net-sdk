4e270fffa5b69af78adb016863af25ec7bb42e9d
Rithik Banerjee <banerjee13rithik@gmail.com>
2020-10-25 09:39:50 +0530

    [feature] comment helper

--- GSTAPI/Services/GSTR7Service.cs
+++ GSTAPI/Services/GSTR7Service.cs
@@ -4,10 +4,12 @@ using System.Collections.Specialized;
 
 namespace GSTAPI.Services
 {
+    //class to call all gstr7 based api 
     public static class GSTR7Service
     {
         private static readonly string ReturnType = "R7";
         private static readonly string Version = UrlHandler.GetVersion(version.v1_1);
+        //file with EVC api
         public static Response FileWithEVC(Request userInfo, string jsonData, string PAN, string OTP)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -17,6 +19,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr7);
             return handler.File(url, jsonData, Version, ReturnType, $"{PAN}|{OTP}");
         }
+        //file with DSC api
         public static Response FileWithDSC(Request userInfo, string jsonData, string signature, string PAN)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -26,6 +29,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr7);
             return handler.File(url, jsonData, Version, ReturnType, PAN, signature);
         }
+        //save api
         public static Response Save(Request userInfo, string jsonData)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -35,6 +39,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr7);
             return handler.Save(url, jsonData);
         }
+        //get TDS checksum api
         public static Response GetTDSChecksum(Request userInfo, string returnPeriod, string gstin, string fromTime = "", string recType = "")
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -53,6 +58,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr7);
             return handler.DecryptGetResponse(url, queryString);
         }
+        //get TDS details api
         public static Response GetTDSDetails(Request userInfo, string returnPeriod, string gstin, string fromTime = "", string recType = "")
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
