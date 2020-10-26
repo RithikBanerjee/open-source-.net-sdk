940dbe3ce42d394b507ac211bcd6eafebae985ff
Rithik Banerjee <banerjee13rithik@gmail.com>
2020-10-26 10:31:21 +0530

    [feature] comment helper

--- GSTAPI/Services/GSTR9CService.cs
+++ GSTAPI/Services/GSTR9CService.cs
@@ -6,10 +6,12 @@ using System.Collections.Specialized;
 
 namespace GSTAPI.Services
 {
+    //class to call all gstr9c based api 
     public static class GSTR9CService
     {
         private static readonly string ReturnType = "R9C";
         private static readonly string Version = UrlHandler.GetVersion(version.v1_1);
+        //save api
         public static Response Save(Request userInfo, string jsonData)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -19,6 +21,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr9c);
             return handler.Save(url, jsonData);
         }
+        //get table 9 for filing 9c api
         public static Response Get9RecordsFor9C(Request userInfo, string returnPeriod, string gstin)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -33,6 +36,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr9c);
             return handler.DecryptGetResponse(url, queryString);
         }
+        //get summary api
         public static Response GetSummary(Request userInfo, string returnPeriod, string gstin)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -47,6 +51,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr9c);
             return handler.DecryptGetResponse(url, queryString);
         }
+        //generate certificate api
         public static Response GenerateCertificate(Request userInfo, string jsonData)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -71,6 +76,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr9c);
             return handler.Put(url, "RETGENCERT", payload);
         }
+        //file with EVC api
         public static Response FileWithEVC(Request userInfo, string jsonData, string PAN, string OTP)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -80,6 +86,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr9c);
             return handler.File(url, jsonData, Version, ReturnType, $"{PAN}|{OTP}");
         }
+        //file with DSC api
         public static Response FileWithDSC(Request userInfo, string jsonData, string signature, string PAN)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -89,6 +96,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_gstr9c);
             return handler.File(url, jsonData, Version, ReturnType, PAN, signature);
         }
+        //generate hash api
         public static Response HashGenerator(Request userInfo, string jsonData)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
