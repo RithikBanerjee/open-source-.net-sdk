3f2fdae614774eea46157d225145ea7231cefe53
Rithik Banerjee <banerjee13rithik@gmail.com>
2020-10-28 09:51:49 +0530

    [feature] comment helper

--- GSTAPI/Services/ITC04Service.cs
+++ GSTAPI/Services/ITC04Service.cs
@@ -4,10 +4,12 @@ using System.Collections.Specialized;
 
 namespace GSTAPI.Services
 {
+    //class to call all itc04 based api 
     public static class ITC04Service
     {
         private static readonly string ReturnType = "ITC04";
         private static readonly string Version = UrlHandler.GetVersion(version.v1_1);
+        //save api
         public static Response Save(Request userInfo, string jsonData)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -17,6 +19,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_itc04);
             return handler.Save(url, jsonData);
         }
+        //file with EVC api
         public static Response FileWithEVC(Request userInfo, string jsonData, string PAN, string OTP)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -26,6 +29,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_itc04);
             return handler.File(url, jsonData, Version, ReturnType, $"{PAN}|{OTP}");
         }
+        //file with DSC api
         public static Response FileWithDSC(Request userInfo, string jsonData, string signature, string PAN)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -35,6 +39,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_itc04);
             return handler.File(url, jsonData, Version, ReturnType, PAN, signature);
         }
+        //get summary api
         public static Response GetSummary(Request userInfo, string returnPeriod, string gstin)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
@@ -49,6 +54,7 @@ namespace GSTAPI.Services
             var url = UrlHandler.Route(accessGroup.taxpayerapi, version.v1_1, modName.returns_itc04);
             return handler.DecryptGetResponse(url, queryString);
         }
+        //get data api
         public static Response GetInvoices(Request userInfo, string returnPeriod, string gstin)
         {
             if (!RequestHandler.IsRequestNull(userInfo, out string message))
